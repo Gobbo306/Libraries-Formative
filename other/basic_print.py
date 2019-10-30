9a1c9b97250f850223fcdbd9e8b12b96da656f52
Gobbo306 <54857960+Gobbo306@users.noreply.github.com>
2019-10-30 10:17:52 -0600

    Add files via upload

---
+++ other/basic_print.py
@@ -0,0 +1,17 @@
+#------------------------------------------------
+#Dylan Friesen
+#other > basic_print
+#October 28, 2019
+#------------------------------------------------
+
+#-------------Lists & Dictionaries-------------
+
+cool_list = []
+
+#-------------------Functions------------------
+
+def choose_basic():
+    list_add = input("What do you want to add to the list?")
+    cool_list.append(list_add)
+    
+john = "true"
